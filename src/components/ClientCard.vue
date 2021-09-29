d028aaf5eb8508e558a137fa819d3d0a58a0cc5c
Clara <clara@claratoloba.com>
2021-09-29 10:10:44 +0200

    wip styles cardClient

--- src/components/ClientCard.vue
+++ src/components/ClientCard.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="card-container">
     <div class="card" v-for="(patient, index) in patients" :key="index">
-      <figure>
-        <img src="@/assets/icons/avatar.svg" />
+      <div class="card-profile">
+        <figure>
+          <img src="@/assets/icons/avatar.svg" class="avatar" />
+        </figure>
         <figcaption>
           <span>
             {{ patient.datos_paciente.nombre }},
             {{ patient.datos_paciente.apellidos }}
           </span>
+          <span class="birthday">
+            <img src="@/assets/icons/calendar.svg" />
+            {{ patient.datos_paciente.fecha_nacimiento }}
+          </span>
         </figcaption>
-      </figure>
+      </div>
       <div class="card-info">
-        <span class="birthday">
-          <img src="@/assets/icons/calendar.svg" />
-          {{ patient.datos_paciente.fecha_nacimiento }}
-        </span>
         <span>
           {{ patient.ficha_dental.clinica }}
         </span>
@@ -30,7 +32,7 @@
           <div v-if="patient.ficha_dental.estado === 'enviado'" class="status-green"> Enviado </div>
           <div v-if="patient.ficha_dental.estado === 'aceptado'" class="status-green"> Aceptado </div>
         </span>
-        <select name="" id="" class="actions">
+        <select name="" id="">
           <option value="" disabled>Acciones</option>
           <option value="edit">Editar</option>
           <option value="finish">Finalizar</option>
@@ -63,20 +65,48 @@ export default {
   justify-content: space-between;
 }
 .card {
-  width: 30%;
+  width: 29%;
   padding: 1rem;
   margin: 1rem 0;
   background-color: white;
   display: flex;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+}
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+.card-profile {
+  padding-right: 1rem;
 }
 figure {
+  width: 3.2rem;
+  height: 3.2rem;
   display: flex;
   flex-direction: column;
   align-items: center;
   flex-shrink: 0;
+  flex: 1;
+}
+.avatar {
+  width: 3.2rem;
+  height: 3.2rem;
+}
+
+figcaption {
+  flex-shrink: 0;
+  flex: 1;
 }
 .card-info {
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  flex: 2;
+}
+.card select {
+  max-width: 6.2rem;
+  border-radius: 5px;
+  padding: 0.4rem;
+  font-size: 1rem;
 }
 </style>
